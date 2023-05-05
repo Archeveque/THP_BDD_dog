5f346983ed992659c23fa3282cd85bbbcced469d
Archeveque <114493055+Archeveque@users.noreply.github.com>
2023-05-05 10:02:25 +0200

    Add files via upload

--- db/schema.rb
+++ db/schema.rb
@@ -10,38 +10,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_04_200510) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_04_153523) do
   create_table "cities", force: :cascade do |t|
     t.string "name"
-    t.string "zip_code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "gossips", force: :cascade do |t|
-    t.integer "user_id"
-    t.string "title"
-    t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_gossips_on_user_id"
-  end
-
-  create_table "tags", force: :cascade do |t|
-    t.string "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
+  create_table "dogs", force: :cascade do |t|
+    t.string "name"
     t.integer "city_id"
-    t.string "pseudo"
-    t.text "description"
-    t.string "email"
-    t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["city_id"], name: "index_users_on_city_id"
+    t.index ["city_id"], name: "index_dogs_on_city_id"
+  end
+
+  create_table "dogsitters", force: :cascade do |t|
+    t.integer "city_id"
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["city_id"], name: "index_dogsitters_on_city_id"
+  end
+
+  create_table "strolls", force: :cascade do |t|
+    t.datetime "date"
+    t.integer "dogsitter_id"
+    t.integer "dog_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["dog_id"], name: "index_strolls_on_dog_id"
+    t.index ["dogsitter_id"], name: "index_strolls_on_dogsitter_id"
   end
 
 end
