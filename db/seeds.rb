c4621a887b584a1b6a208b12082cf3257182a774
Archeveque <114493055+Archeveque@users.noreply.github.com>
2023-05-05 09:23:04 +0200

    added seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,41 +5,26 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
 require 'faker'
 City.destroy_all
-Dog.destroy_all
-Dogsitter.destroy_all
-Stroll.destroy_all
+User.destroy_all
+Gossip.destroy_all
+#Tags.destroy_all
 puts "Old data deleted"
 paris = City.create(name: "Paris")
 nice = City.create(name: " Nice")
-3.times do
+7.times do
   City.create(name: Faker::Games::SuperSmashBros.stage)
 end
-  sitter1 = Dogsitter.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name,city: paris)
-  sitter2 = Dogsitter.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name,city: nice)
-  sitter3 = Dogsitter.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name,city: paris)
-  7.times do
-    sitter = Dogsitter.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name,city: paris)
-  end
-puts " dogsitters done"
-  dog1 = Dog.create!(name:"Nihilus le dévoreur de mondes",city: paris)
-  dog2 = Dog.create!(name: Faker::Name.first_name,city: nice)
-  dog3 = Dog.create!(name: Faker::Name.first_name,city: paris)
-  7.times do
-    dog = Dog.create!(name: Faker::Name.first_name,city: paris)
-  end
+  user1 = User.create!(pseudo: Faker::Name.first_name,description: Faker::Lorem.sentence(word_count: 3, random_words_to_add: 6),email: Faker::Internet.email,age: rand(8..80),city: paris)
+  user2 = User.create!(pseudo: Faker::Name.first_name,description: Faker::Lorem.sentence(word_count: 3, random_words_to_add: 6),email: Faker::Internet.email,age: rand(8..80),city: paris)
+  user3 = User.create!(pseudo: Faker::Name.first_name,description: Faker::Lorem.sentence(word_count: 3, random_words_to_add: 6),email: Faker::Internet.email,age: rand(8..80),city: paris)
 
-  puts "Stroll1 : Dog 'sitter1' et dog1"
-  stroll1 = Stroll.create!(dogsitter: sitter1,dog: dog1)
-  puts "Stroll2 : Dog 'sitter2' et dog2"
-  stroll2 = Stroll.create!(dogsitter: sitter2,dog: dog2)
-  puts "Stroll3 : Dog 'sitter3' et dog3"
-  stroll3 = Stroll.create!(dogsitter: sitter3,dog: dog3)
-  puts "Stroll4 : Dog 'sitter2' et dog1"
-  stroll4 = Stroll.create!(dogsitter: sitter2,dog: dog1)
-  puts "Stroll5 : Dog 'sitter2' et dog3"
-  stroll5 = Stroll.create!(dogsitter: sitter2,dog: dog3)
-  puts "Stroll6 : Dog 'sitter3' et dog1"
-  stroll6 = Stroll.create!(dogsitter: sitter3,dog: dog1)
-
+  7.times do
+    user = User.create!(pseudo: Faker::Name.first_name,description: Faker::Lorem.sentence,email: Faker::Internet.email,age: rand(8..80),city: paris)
+  end
+puts " users done"
+  20.times do
+    gossip = Gossip.create!(title: Faker::Name.first_name,content: Faker::Lorem.sentence(word_count: 3, random_words_to_add: 6),user: User.all.sample)
+  end
