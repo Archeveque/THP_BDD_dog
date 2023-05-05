aba0cf55493ae0991624d8203d2f5faef69d17df
Archeveque <114493055+Archeveque@users.noreply.github.com>
2023-05-05 08:56:17 +0200

    Added seeds.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,31 +10,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_04_143606) do
-  create_table "appointments", force: :cascade do |t|
+ActiveRecord::Schema[7.0].define(version: 2023_05_04_153523) do
+  create_table "cities", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "dogs", force: :cascade do |t|
+    t.string "name"
+    t.integer "city_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["city_id"], name: "index_dogs_on_city_id"
+  end
+
+  create_table "dogsitters", force: :cascade do |t|
+    t.integer "city_id"
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["city_id"], name: "index_dogsitters_on_city_id"
+  end
+
+  create_table "strolls", force: :cascade do |t|
     t.datetime "date"
-    t.integer "doctor_id"
-    t.integer "patient_id"
+    t.integer "dogsitter_id"
+    t.integer "dog_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["doctor_id"], name: "index_appointments_on_doctor_id"
-    t.index ["patient_id"], name: "index_appointments_on_patient_id"
-  end
-
-  create_table "doctors", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "speciality"
-    t.string "zip_code"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "patients", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.index ["dog_id"], name: "index_strolls_on_dog_id"
+    t.index ["dogsitter_id"], name: "index_strolls_on_dogsitter_id"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,33 +6,40 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 require 'faker'
-Doctor.destroy_all
-Patient.destroy_all
-Appointment.destroy_all
+City.destroy_all
+Dog.destroy_all
+Dogsitter.destroy_all
+Stroll.destroy_all
 puts "Old data deleted"
-#5.times do |i|
-  doc1 = Doctor.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, speciality: Faker::Game.title)
-  patient1 = Patient.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
-  puts "done"
-  doc2 = Doctor.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, speciality: Faker::Game.title)
-  patient2 = Patient.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
-  puts "done"
-  doc3 = Doctor.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, speciality: Faker::Game.title)
-  patient3 = Patient.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
-  puts "done"
-  puts "Liste rdv:"
-  puts "rdv1 :doc1 et patient1"
-  rdv1 = Appointment.create(doctor: doc1, patient: patient1)
-  puts "rdv2 :doc2 et patient2"
-  rdv2 = Appointment.create(doctor: doc2, patient: patient2)
-  puts "rdv3 :doc3 et patient3"
-  rdv2 = Appointment.create(doctor: doc3, patient: patient3)
-  puts "rdv4 :doc2 et patient3"
-  rdv2 = Appointment.create(doctor: doc2, patient: patient3)
-  puts "rdv5 :doc1 et patient3"
-  rdv2 = Appointment.create(doctor: doc1, patient: patient3)
-  puts "done"
+paris = City.create(name: "Paris")
+nice = City.create(name: " Nice")
+3.times do
+  City.create(name: Faker::Games::SuperSmashBros.stage)
+end
+  sitter1 = Dogsitter.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name,city: paris)
+  sitter2 = Dogsitter.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name,city: nice)
+  sitter3 = Dogsitter.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name,city: paris)
+  7.times do
+    sitter = Dogsitter.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name,city: paris)
+  end
+puts " dogsitters done"
+  dog1 = Dog.create!(name:"Nihilus le dévoreur de mondes",city: paris)
+  dog2 = Dog.create!(name: Faker::Name.first_name,city: nice)
+  dog3 = Dog.create!(name: Faker::Name.first_name,city: paris)
+  7.times do
+    dog = Dog.create!(name: Faker::Name.first_name,city: paris)
+  end
 
-
-
+  puts "Stroll1 : Dog 'sitter1' et dog1"
+  stroll1 = Stroll.create!(dogsitter: sitter1,dog: dog1)
+  puts "Stroll2 : Dog 'sitter2' et dog2"
+  stroll2 = Stroll.create!(dogsitter: sitter2,dog: dog2)
+  puts "Stroll3 : Dog 'sitter3' et dog3"
+  stroll3 = Stroll.create!(dogsitter: sitter3,dog: dog3)
+  puts "Stroll4 : Dog 'sitter2' et dog1"
+  stroll4 = Stroll.create!(dogsitter: sitter2,dog: dog1)
+  puts "Stroll5 : Dog 'sitter2' et dog3"
+  stroll5 = Stroll.create!(dogsitter: sitter2,dog: dog3)
+  puts "Stroll6 : Dog 'sitter3' et dog1"
+  stroll6 = Stroll.create!(dogsitter: sitter3,dog: dog1)
 
